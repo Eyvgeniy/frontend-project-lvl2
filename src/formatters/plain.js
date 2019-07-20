5efd6d9199132221d638a27e77f1b90be5a14ad6
Evgeniy <antonm33@yandex.ru>
2019-07-20 22:11:01 +0200

    fix long line errors

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,6 +1,7 @@
 import { isObject } from 'lodash';
 
-const renderObjectValue = value => (isObject(value) ? '[complex value]' : `${value}`);
+const renderObjectValue = value =>
+  isObject(value) ? '[complex value]' : `${value}`;
 
 const typeActions = [
   {
@@ -11,42 +12,47 @@ const typeActions = [
   {
     name: 'unchanged',
     check: arg => arg === 'unchanged',
-    process: (key, oldValue, newValue, acc) => `Property '${acc}${key}' was not changed`,
+    process: (key, oldValue, newValue, acc) =>
+      `Property '${acc}${key}' was not changed`,
   },
   {
     name: 'changed',
     check: arg => arg === 'changed',
-    process: (key, oldValue, newValue, acc) => `Property '${acc}${key}' was updated. From '${renderObjectValue(
-      oldValue,
-    )}' to '${renderObjectValue(newValue)}'`,
+    process: (key, oldValue, newValue, acc) =>
+      `Property '${acc}${key}' was updated. From '${renderObjectValue(
+        oldValue,
+      )}' to '${renderObjectValue(newValue)}'`,
   },
   {
     name: 'deleted',
     check: arg => arg === 'deleted',
-    process: (key, oldValue, newValue, acc) => `Property '${acc}${key}' was removed`,
+    process: (key, oldValue, newValue, acc) =>
+      `Property '${acc}${key}' was removed`,
   },
   {
     name: 'added',
     check: arg => arg === 'added',
-    process: (key, oldValue, newValue, acc) => `Property '${acc}${key}' was added with value: '${renderObjectValue(newValue)}'`,
+    process: (key, oldValue, newValue, acc) =>
+      `Property '${acc}${key}' was added with value: '${renderObjectValue(
+        newValue,
+      )}'`,
   },
 ];
 
 const getTypeAction = arg => typeActions.find(({ check }) => check(arg));
 
-const render = (ast) => {
-  const iter = (astDiff, acc) => astDiff
-    .map((node) => {
-      const {
-        key, oldValue, newValue, children, type,
-      } = node;
-      const path = `${acc}${key}.`;
-      const { process } = getTypeAction(type);
-      return children.length > 0
-        ? process(iter, children, path)
-        : process(key, oldValue, newValue, acc);
-    })
-    .join('\n');
+const render = ast => {
+  const iter = (astDiff, acc) =>
+    astDiff
+      .map(node => {
+        const { key, oldValue, newValue, children, type } = node;
+        const path = `${acc}${key}.`;
+        const { process } = getTypeAction(type);
+        return children.length > 0
+          ? process(iter, children, path)
+          : process(key, oldValue, newValue, acc);
+      })
+      .join('\n');
 
   return iter(ast, '');
 };
--- src/formatters/tree.js
+++ src/formatters/tree.js
@@ -1,65 +1,70 @@
-import {
-  isObject,
-} from 'lodash';
+import { isObject } from 'lodash';
 
 const indent = '  ';
 
 const renderObjectValue = (value, deep) => {
   if (isObject(value)) {
-    const values = Object.keys(value).map(el => `  ${el}: ${value[el]}`).join('\n');
-    return `{\n${indent.repeat(deep + 2)}${values}\n${indent.repeat(deep + 1)}}`;
+    const values = Object.keys(value)
+      .map(el => `  ${el}: ${value[el]}`)
+      .join('\n');
+    return `{\n${indent.repeat(deep + 2)}${values}\n${indent.repeat(
+      deep + 1,
+    )}}`;
   }
   return `${value}`;
 };
 
-const typeActions = [{
-  name: 'nested',
-  check: arg => arg === 'nested',
-  process: (key, render, children, deep) => `${indent.repeat(deep - 1)}${key}: {\n${render(children, deep)}\n${indent.repeat(deep - 1)}}`,
-},
-{
-  name: 'unchanged',
-  check: arg => arg === 'unchanged',
-  process: (key, oldValue, newValue, deep) => `${indent.repeat(deep)}  ${key}: ${oldValue}`,
-},
-{
-  name: 'changed',
-  check: arg => arg === 'changed',
-  process: (key, oldValue, newValue, deep) => [
-    [`${indent.repeat(deep)}- ${key}: ${renderObjectValue(oldValue, deep)}`],
-    [`${indent.repeat(deep)}+ ${key}: ${renderObjectValue(newValue, deep)}`],
-  ],
-},
-{
-  name: 'deleted',
-  check: arg => arg === 'deleted',
-  process: (key, oldValue, newValue, deep) => `${indent.repeat(deep)}- ${key}: ${renderObjectValue(oldValue, deep)}`,
-},
-{
-  name: 'added',
-  check: arg => arg === 'added',
-  process: (key, oldValue, newValue, deep) => `${indent.repeat(deep)}+ ${key}: ${renderObjectValue(newValue, deep)}`,
-},
+const typeActions = [
+  {
+    name: 'nested',
+    check: arg => arg === 'nested',
+    process: (key, render, children, deep) =>
+      `${indent.repeat(deep - 1)}${key}: {\n${render(
+        children,
+        deep,
+      )}\n${indent.repeat(deep - 1)}}`,
+  },
+  {
+    name: 'unchanged',
+    check: arg => arg === 'unchanged',
+    process: (key, oldValue, newValue, deep) =>
+      `${indent.repeat(deep)}  ${key}: ${oldValue}`,
+  },
+  {
+    name: 'changed',
+    check: arg => arg === 'changed',
+    process: (key, oldValue, newValue, deep) => [
+      [`${indent.repeat(deep)}- ${key}: ${renderObjectValue(oldValue, deep)}`],
+      [`${indent.repeat(deep)}+ ${key}: ${renderObjectValue(newValue, deep)}`],
+    ],
+  },
+  {
+    name: 'deleted',
+    check: arg => arg === 'deleted',
+    process: (key, oldValue, newValue, deep) =>
+      `${indent.repeat(deep)}- ${key}: ${renderObjectValue(oldValue, deep)}`,
+  },
+  {
+    name: 'added',
+    check: arg => arg === 'added',
+    process: (key, oldValue, newValue, deep) =>
+      `${indent.repeat(deep)}+ ${key}: ${renderObjectValue(newValue, deep)}`,
+  },
 ];
-const getTypeAction = arg => typeActions.find(({
-  check,
-}) => check(arg));
+const getTypeAction = arg => typeActions.find(({ check }) => check(arg));
 
-const diff = (astDiff) => {
-  const render = (ast, deep) => ast.map((el) => {
-    const {
-      key,
-      oldValue,
-      newValue,
-      type,
-      children,
-    } = el;
-    const {
-      process,
-    } = getTypeAction(type);
-    return children.length > 0 ? process(key, render, children, deep + 2)
-      : process(key, oldValue, newValue, deep);
-  }).flat().join('\n');
+const diff = astDiff => {
+  const render = (ast, deep) =>
+    ast
+      .map(el => {
+        const { key, oldValue, newValue, type, children } = el;
+        const { process } = getTypeAction(type);
+        return children.length > 0
+          ? process(key, render, children, deep + 2)
+          : process(key, oldValue, newValue, deep);
+      })
+      .flat()
+      .join('\n');
   return `{\n${render(astDiff, 0)}\n}`;
 };
 
