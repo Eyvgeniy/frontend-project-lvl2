a5d3e7e94b09b9d9be38fe5932af7d0a3e0f79ed
Eyvgeniy <antonm33@yandex.ru>
2019-07-28 19:38:40 +0200

    fix import and variable

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,16 +1,15 @@
 #!/usr/bin/env node
 
-import gendiff from '../index';
+import program from "commander";
+import gendiff from "../index";
 
-const program = require('commander');
-
-const diff = program
-  .description('Compares two configuration files and shows a difference.')
-  .arguments('<firstconfig> <secondconfig>')
-  .option('-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'Output format')
+program
+  .description("Compares two configuration files and shows a difference.")
+  .arguments("<firstconfig> <secondconfig>")
+  .option("-V, --version", "output the version number")
+  .option("-f, --format <type>", "Output format")
   .action((firstconfig, secondconfig) =>
-    console.log(gendiff(firstconfig, secondconfig, program.format)),
+    console.log(gendiff(firstconfig, secondconfig, program.format))
   );
 
 diff.parse(process.argv);
