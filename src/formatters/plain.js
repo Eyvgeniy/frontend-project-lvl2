eefa731c7244a4c48d044396b2b2a89dde64fe74
Eyvgeniy <antonm33@yandex.ru>
2019-07-28 20:26:50 +0200

    change obj typeAction to key-valye pairs

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,60 +1,32 @@
-import { isObject } from 'lodash';
+import { isObject } from "lodash";
 
-const renderObjectValue = value =>
-  isObject(value) ? '[complex value]' : `${value}`;
+const renderObjectValue = value => (isObject(value) ? "[complex value]" : `${value}`);
 
-const typeActions = [
-  {
-    name: 'nested',
-    check: arg => arg === 'nested',
-    process: (render, children, acc) => render(children, acc)
-  },
-  {
-    name: 'unchanged',
-    check: arg => arg === 'unchanged',
-    process: (key, oldValue, newValue, acc) =>
-      `Property '${acc}${key}' was not changed`
-  },
-  {
-    name: 'changed',
-    check: arg => arg === 'changed',
-    process: (key, oldValue, newValue, acc) =>
-      `Property '${acc}${key}' was updated. From '${renderObjectValue(
-        oldValue
-      )}' to '${renderObjectValue(newValue)}'`
-  },
-  {
-    name: 'deleted',
-    check: arg => arg === 'deleted',
-    process: (key, oldValue, newValue, acc) =>
-      `Property '${acc}${key}' was removed`
-  },
-  {
-    name: 'added',
-    check: arg => arg === 'added',
-    process: (key, oldValue, newValue, acc) =>
-      `Property '${acc}${key}' was added with value: '${renderObjectValue(
-        newValue
-      )}'`
-  }
-];
-
-const getTypeAction = arg => typeActions.find(({ check }) => check(arg));
-
+const typeActions = {
+  nested: (render, children, acc) => render(children, acc),
+  unchanged: (key, oldValue, newValue, acc) => `Property '${acc}${key}' was not changed`,
+  changed: (key, oldValue, newValue, acc) =>
+    `Property '${acc}${key}' was updated. From '${renderObjectValue(
+      oldValue
+    )}' to '${renderObjectValue(newValue)}'`,
+  deleted: (key, oldValue, newValue, acc) => `Property '${acc}${key}' was removed`,
+  added: (key, oldValue, newValue, acc) =>
+    `Property '${acc}${key}' was added with value: '${renderObjectValue(newValue)}'`
+};
 const render = ast => {
   const iter = (astDiff, acc) =>
     astDiff
       .map(node => {
         const { key, oldValue, newValue, children, type } = node;
         const path = `${acc}${key}.`;
-        const { process } = getTypeAction(type);
+        const process = typeActions[type];
         return children.length > 0
           ? process(iter, children, path)
           : process(key, oldValue, newValue, acc);
       })
-      .join('\n');
+      .join("\n");
 
-  return iter(ast, '');
+  return iter(ast, "");
 };
 
 export default render;
