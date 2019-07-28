dc0fe6f54ceaf1c67047cae23b6c5e870fc37a4d
Eyvgeniy <antonm33@yandex.ru>
2019-07-28 19:22:17 +0200

    change tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,25 +1,20 @@
-import fs from 'fs';
-import gendiff from '../src';
+import fs from "fs";
+import path from "path";
+import gendiff from "../src";
 
-const path = `${__dirname}/__fixtures__`;
-const result = fs.readFileSync(`${path}/result`, 'utf-8');
-const resultNested = fs.readFileSync(`${path}/resultnested`, 'utf-8');
-const plain = fs.readFileSync(`${path}/plain`, 'utf-8');
-const plainnested = fs.readFileSync(`${path}/plainnested`, 'utf-8');
+const exts = ["json", "yaml", "ini"];
+const formats = ["tree", "plain"];
 
-test.each([
-  ['json', result, `${path}/json/before.json`, `${path}/json/after.json`],
-  ['plain', plain, `${path}/json/before.json`, `${path}/json/after.json`],
-  ['json', result, `${path}/yaml/before.yaml`, `${path}/yaml/after.yaml`],
-  ['plain', plain, `${path}/yaml/before.yaml`, `${path}/yaml/after.yaml`],
-  ['json', result, `${path}/ini/before.ini`, `${path}/ini/after.ini`],
-  ['plain', plain, `${path}/ini/before.ini`, `${path}/ini/after.ini`],
-  ['json', resultNested, `${path}/json/beforenested.json`, `${path}/json/afternested.json`],
-  ['json', resultNested, `${path}/yaml/beforenested.yaml`, `${path}/yaml/afternested.yaml`],
-  ['json', resultNested, `${path}/ini/beforenested.ini`, `${path}/ini/afternested.ini`],
-  ['plain', plainnested, `${path}/json/beforenested.json`, `${path}/json/afternested.json`],
-  ['plain', plainnested, `${path}/yaml/beforenested.yaml`, `${path}/yaml/afternested.yaml`],
-  ['plain', plainnested, `${path}/ini/beforenested.ini`, `${path}/ini/afternested.ini`],
-])('check diff, type of output: %p', (format, expected, a, b) => {
-  expect(gendiff(a, b, format)).toBe(expected);
+const nested = exts.map(ext => formats.map(format => ["nested", ext, format])).flat();
+const flat = exts.map(ext => formats.map(format => ["flat", ext, format])).flat();
+
+const getPath = (fileName, ext) => path.join(__dirname, "__fixtures__", ext, `${fileName}.${ext}`);
+
+test.each([...flat, ...nested])("check diff, type of output: %p", (type, ext, format) => {
+  const firstFile = getPath(`before${type}`, ext);
+  const secondFile = getPath(`after${type}`, ext);
+  const resultPath = path.join(`__fixtures__`, `${format}${type}`);
+  const expected = fs.readFileSync(`${__dirname}/${resultPath}`, "utf-8");
+
+  expect(gendiff(firstFile, secondFile, format)).toBe(expected);
 });
