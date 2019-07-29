d08551835c1f63628062ed7db57736af1672580d
Eyvgeniy <antonm33@yandex.ru>
2019-07-29 21:54:09 +0200

    fix varables names and remove unnesessary options from ast node

--- src/index.js
+++ src/index.js
@@ -6,31 +6,25 @@ const buildAst = (first, second) => {
   const keys = union(Object.keys(first), Object.keys(second));
   const ast = keys.map(key => {
     if (has(first, key) && has(second, key) && isObject(first[key]) && isObject(second[key])) {
-      return {
-        key,
-        oldValue: null,
-        newValue: null,
-        type: "nested",
-        children: buildAst(first[key], second[key])
-      };
+      return { key, type: "nested", children: buildAst(first[key], second[key]) };
     }
     if (has(first, key) && has(second, key) && first[key] === second[key]) {
-      return { key, oldValue: first[key], newValue: null, type: "unchanged", children: [] };
+      return { key, oldValue: first[key], type: "unchanged" };
     }
     if (has(first, key) && has(second, key) && first[key] !== second[key]) {
-      return { key, oldValue: first[key], newValue: second[key], type: "changed", children: [] };
+      return { key, oldValue: first[key], newValue: second[key], type: "changed" };
     }
     if (has(first, key) && !has(second, key)) {
-      return { key, oldValue: first[key], newValue: null, type: "deleted", children: [] };
+      return { key, oldValue: first[key], type: "deleted" };
     }
-    return { key, oldValue: null, newValue: second[key], type: "added", children: [] };
+    return { key, newValue: second[key], type: "added" };
   });
   return ast;
 };
 
-const gendiff = (firstFile, secondFile, format) => {
-  const firstData = parse(firstFile);
-  const secondData = parse(secondFile);
+const gendiff = (firstFilePath, secondFilePath, format) => {
+  const firstData = parse(firstFilePath);
+  const secondData = parse(secondFilePath);
   const ast = buildAst(firstData, secondData);
   return render(ast, format);
 };
