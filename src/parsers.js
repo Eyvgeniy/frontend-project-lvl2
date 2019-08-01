e94520376f492035f3ff14f90f7f930c94978818
Eyvgeniy <antonm33@yandex.ru>
2019-08-01 08:01:40 +0200

    fix error with obj typeFiles

--- src/parsers.js
+++ src/parsers.js
@@ -1,20 +1,13 @@
-import ini from "ini";
-import yaml from "js-yaml";
-import fs from "fs";
-import path from "path";
+import ini from 'ini';
+import yaml from 'js-yaml';
+import fs from 'fs';
+import path from 'path';
 
-const typeFile = [
-  {
-    ".yaml": filePath => yaml.safeLoad(fs.readFileSync(`${filePath}`, "utf8"))
-  },
-  {
-    ".json": filePath => JSON.parse(fs.readFileSync(`${filePath}`, "utf8"))
-  },
-  {
-    ".ini": filePath => ini.parse(fs.readFileSync(`${filePath}`, "utf-8"))
-  }
-];
-
+const typeFile = {
+  '.yaml': filePath => yaml.safeLoad(fs.readFileSync(`${filePath}`, 'utf8')),
+  '.json': filePath => JSON.parse(fs.readFileSync(`${filePath}`, 'utf8')),
+  '.ini': filePath => ini.parse(fs.readFileSync(`${filePath}`, 'utf-8'))
+};
 export default filePath => {
   const ext = path.extname(filePath);
   const process = typeFile[ext];
