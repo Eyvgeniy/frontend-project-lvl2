6e461bf1fecf79ff1a264d8b87b4ca40e5b2f073
Eyvgeniy <antonm33@yandex.ru>
2019-08-01 08:01:00 +0200

    fix import of index

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import program from "commander";
-import gendiff from "../index";
+import gendiff from "..";
 
 program
   .description("Compares two configuration files and shows a difference.")
